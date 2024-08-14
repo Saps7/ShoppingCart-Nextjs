b46c416a7333133479abd825a71f9faf5f1f3adc
Saps7 <saptarshisg.2000@gmail.com>
2024-08-15 02:51:39 +0530

    Formatted Price

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import { formatCurrency } from '@/utilities/formatCurrency';
 import React from 'react';
 
 type ProductProps = {
@@ -16,8 +17,8 @@ export default function ProductCard({ id, name, price, imgUrl, discount }: Produ
         <div className="p-4">
           <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">{name}</h2>
           <div className="flex items-center">
-            <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">$20.00</p>
-            <p className="text-base  font-medium text-gray-500 line-through dark:text-gray-300">{}</p>
+            <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">{formatCurrency(price - (discount*price/100))}</p>
+            <p className="text-base  font-medium text-gray-500 line-through dark:text-gray-300">{formatCurrency(price)}</p>
             <p className="ml-auto text-base font-medium text-green-500">{discount}% off</p>
           </div>
         </div>

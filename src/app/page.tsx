c68300a8dfc7d0942aa6aacf9fa03f898cf49343
Saps7 <saptarshisg.2000@gmail.com>
2024-08-15 17:13:05 +0530

    Added Navbar + Minor UI changes in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ProductCard from '@/components/ProductCard';
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-8 lg:gap-12 md:mx-8 lg:mx-12 md:mt-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 lg:gap-5 md:mx-7 lg:mx-12 lg:mt-5">
       {products.map(product => (
         <div key={product.id}>
           <ProductCard {...product} />
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { formatCurrency } from '@/utilities/formatCurrency';
+import { formatCurrency } from '@/lib/formatCurrency';
 import React from 'react';
 
 type ProductProps = {
@@ -12,7 +12,7 @@ type ProductProps = {
 
 export default function ProductCard({ id, name, price, imgUrl, discount }: ProductProps) {
   return (
-    <div className="transform overflow-hidden rounded-none md:rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+    <div className="transform overflow-hidden rounded-none lg:rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
         <img className="h-48 w-full object-cover object-center" alt={name} src={imgUrl}/>
         <div className="p-4">
           <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">{name}</h2>

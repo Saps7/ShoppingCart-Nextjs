ac5632be83f67649a74bb44b0bd1f2dcc9905691
Saps7 <saptarshisg.2000@gmail.com>
2024-08-18 04:10:11 +0530

    minor ui change

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -30,7 +30,7 @@ export default function ProductCard({ id, name, price, imgUrl, discount, inStock
 
   return (
     <>
-      <div className="transform overflow-hidden rounded-none lg:rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+      <div className="transform overflow-hidden rounded-none lg:rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg">
         <img className="h-48 w-full object-cover object-center" alt={name} src={imgUrl} />
         <div className="p-4">
           <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">{name}</h2>

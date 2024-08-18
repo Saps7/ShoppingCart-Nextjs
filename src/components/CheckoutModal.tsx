8e4c3fb290f59b1951c2916aa61bfbe2ec04e1f0
Saps7 <saptarshisg.2000@gmail.com>
2024-08-18 15:37:34 +0530

    removed dark mode styling

--- src/components/CheckoutModal.tsx
+++ src/components/CheckoutModal.tsx
@@ -39,9 +39,15 @@ const CheckoutModal = ({ open, setOpen, total }: CheckoutModalProps) => {
                 </div>
             </div>
             <div className="flex flex-col justify-center items-center">
-                <h3 className="my-4 text-center text-3xl font-semibold text-gray-700">Congratuations!</h3>
-                <p className="w-[250px] text-center font-normal text-gray-600">Your order has been checked out succesfully!</p>
-                <h3 className="w-[250px] text-center font-semibold py-4 mt-4 text-xl text-gray-700">Total: {formatCurrency(total)}</h3>
+                <h3 className="my-4 text-center text-3xl font-semibold text-gray-700">
+                    Congratuations!
+                </h3>
+                <p className="w-[250px] text-center font-normal text-gray-600">
+                    Your order is ready! Confirm the details and place your order.
+                </p>
+                <h3 className="w-[250px] text-center font-semibold py-4 mt-4 text-xl text-gray-700">
+                    Total: {formatCurrency(total)}
+                </h3>
                 <button
                     className="mx-auto mt-5 block rounded-xl border-4 border-transparent bg- px-6 py-3 bg-gray-900 text-center text-base font-medium text-white outline-8 hover:outline hover:duration-300"
                     onClick={handleOrderPlace}
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -33,10 +33,14 @@ export default function ProductCard({ id, name, price, imgUrl, discount, inStock
       <div className="transform overflow-hidden rounded-none lg:rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg">
         <img className="h-48 w-full object-cover object-center" alt={name} src={imgUrl} />
         <div className="p-4">
-          <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">{name}</h2>
+          <h2 className="mb-2 text-lg font-medium text-gray-900">{name}</h2>
           <div className="flex items-center">
-            <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">{formatCurrency(price - (discount * price / 100))}</p>
-            <p className="text-base  font-medium text-gray-500 line-through dark:text-gray-300">{formatCurrency(price)}</p>
+            <p className="mr-2 text-lg font-semibold text-gray-900">
+              {formatCurrency(price - (discount * price / 100))}
+            </p>
+            <p className="text-base  font-medium text-gray-500 line-through">
+              {formatCurrency(price)}
+            </p>
             <p className="ml-auto text-base font-medium text-green-500">{discount}% off</p>
           </div>
           {quantity === 0 ? (
